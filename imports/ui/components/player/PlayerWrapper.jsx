6b979b0f51ab3de431e470430794a71301d25e97
drian <a.poberezhnyk@gmail.com>
2017-03-10 09:49:35 +0200

    player

--- imports/ui/components/player/PlayerWrapper.jsx
+++ imports/ui/components/player/PlayerWrapper.jsx
@@ -5,9 +5,35 @@ import { Bert } from 'meteor/themeteorchef:bert';
 import Player from './Player';
 
 export default class PlayerWrapper extends TrackerReact(Component) {
+  state = {
+    isPlaying: false,
+  };
+  playToggle = () => {
+    if (this.audio.paused) {
+      this.audio.play();
+    } else {
+      this.audio.pause();
+    }
+    this.setState({ isPlaying: !this.audio.paused });
+  };
+  changeVolume = (event, newValue) => {
+    this.audio.volume = newValue;
+  };
   renderPlayer = () => {
     if (Meteor.user()) {
-      return <Player />;
+      return (
+        <div>
+          <Player
+            playToggle={this.playToggle}
+            changeVolume={this.changeVolume}
+            isPlaying={this.state.isPlaying}
+          />;
+          <audio
+            src="http://stream.mjoy.ua:8000/urban-space-radio-aac"
+            ref={(a) => { this.audio = a; }}
+          />
+        </div>
+      );
     }
     return null;
   };
--- imports/ui/components/stations/modal/AddStationModal.jsx
+++ imports/ui/components/stations/modal/AddStationModal.jsx
@@ -1,17 +1,16 @@
 import React, { Component, PropTypes } from 'react';
-import { TextField } from 'material-ui';
+import { TextField, RaisedButton }from 'material-ui';
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 
-export default class AddCategoryModal extends Component {
+export default class AddStationModal extends Component {
   static propTypes = {
     open: PropTypes.bool,
     handleClose: PropTypes.func,
     handleSubmit: PropTypes.func,
-    changeHandle: PropTypes.func,
   };
   render() {
-    const { open, handleClose, handleSubmit, changeHandle } = this.props;
+    const { open, handleClose, handleSubmit } = this.props;
     const actions = [
       <FlatButton
         label="Cancel"
@@ -28,7 +27,7 @@ export default class AddCategoryModal extends Component {
     return (
       <div>
         <Dialog open={open} actions={actions} title="Create new category" onRequestClose={handleClose}>
-          <TextField fullWidth onChange={changeHandle} floatingLabelText="Category name" />
+          <TextField fullWidth floatingLabelText="Category name" />
         </Dialog>
       </div>
     );
