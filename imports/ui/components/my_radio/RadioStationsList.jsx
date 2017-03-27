f990fdd9fa634efbea313dd9b5e3f24b106ce24e
drian <a.poberezhnyk@gmail.com>
2017-03-27 22:56:30 +0300

    space button toggle playing and select station functionality

--- imports/ui/components/my_radio/RadioStationsList.jsx
+++ imports/ui/components/my_radio/RadioStationsList.jsx
@@ -1,24 +1,22 @@
 import React, { Component, PropTypes } from 'react';
-import IconButton from 'material-ui/IconButton';
-import PlayCircleFilled from 'material-ui/svg-icons/av/play-circle-filled';
-import PauseCircleFilled from 'material-ui/svg-icons/av/pause-circle-filled';
 
 export default class RadioStationsList extends Component {
   static propTypes = {
     stations: PropTypes.array,
+    isPlaying: PropTypes.bool,
+    selectStation: PropTypes.func,
+    nowPlaying: PropTypes.string,
   };
   render() {
     return (
       <table className="station-tbl">
         <tbody>
           {this.props.stations.map(station => (
-            <tr key={station._id}>
-              <td>
-                <IconButton className="play-btn" onTouchTap={() => console.log(station.stationsUrl)}>
-                    {/*<PauseCircleFilled color={'#00BCD4'} className="play-ico" /> :*/}
-                    <PlayCircleFilled color={'#00BCD4'} className="play-ico" />
-                </IconButton>
-              </td>
+            <tr
+              className={this.props.nowPlaying === station.stationsUrl ? 'selected' : ''}
+              key={station._id}
+              onTouchTap={() => this.props.selectStation(station.stationsUrl)}
+            >
               <td>{station.stationsName}</td>
               <td>{station.stationsUrl}</td>
             </tr>
--- imports/ui/components/player/PlayerWrapper.jsx
+++ imports/ui/components/player/PlayerWrapper.jsx
@@ -1,10 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
-import { Bert } from 'meteor/themeteorchef:bert';
 import Player from './Player';
 
 export default class PlayerWrapper extends TrackerReact(Component) {
+
   renderPlayer = () => {
     if (Meteor.user()) {
       return (
